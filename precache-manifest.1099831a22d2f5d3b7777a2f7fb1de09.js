ad67a780132b147cdd2aedcb79aa49a8b9075c66
claudio.donoso <claudiodonosoc@gmail.com>
2024-02-13 22:41:52 -0300

    Updates

---
+++ precache-manifest.1099831a22d2f5d3b7777a2f7fb1de09.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "39581ef383d8d3e61dd4147ec40a8fea",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "1262875a0e8b6ce9e460",
+    "url": "/react-portfolio/static/css/2.bfdd77b9.chunk.css"
+  },
+  {
+    "revision": "1bcb56c35e5708195d73",
+    "url": "/react-portfolio/static/css/main.2bc2e7ef.chunk.css"
+  },
+  {
+    "revision": "1262875a0e8b6ce9e460",
+    "url": "/react-portfolio/static/js/2.e99a2d2c.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/react-portfolio/static/js/2.e99a2d2c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1bcb56c35e5708195d73",
+    "url": "/react-portfolio/static/js/main.6eb18a26.chunk.js"
+  },
+  {
+    "revision": "aff33cef9dbb8787d6b3",
+    "url": "/react-portfolio/static/js/runtime-main.213b1880.js"
+  }
+]);
